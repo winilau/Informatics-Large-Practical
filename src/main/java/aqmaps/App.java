d2e4524cf8b26ac9f049262734182556db4edb60
Wini <winilau22@gmail.com>
2020-11-30 19:49:35 +0000

    yay

--- src/main/java/aqmaps/App.java
+++ src/main/java/aqmaps/App.java
@@ -8,6 +8,5 @@ public class App {
 	public static void main(String[] args) throws IOException, InterruptedException{
 		var path = new Path(args[6], args[2], args[1], args[0], args[3], args[4]);
 		List<LineString> finalPath = path.findPath();
-		
 	}
 }
