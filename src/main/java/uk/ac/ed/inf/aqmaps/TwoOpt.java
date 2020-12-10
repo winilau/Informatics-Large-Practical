262190d97481fa1d5ab2ac5d6f483b60e412519a
winilau22@gmail.com <winilau22@gmail.com>
2020-12-11 00:17:27 +0800

    finallyyy

--- src/main/java/uk/ac/ed/inf/aqmaps/TwoOpt.java
+++ src/main/java/uk/ac/ed/inf/aqmaps/TwoOpt.java
@@ -4,7 +4,10 @@ import java.util.*;
 import com.mapbox.geojson.Point;
 
 /**
- * This class is used to find the final path of using the 2 opt heuristic
+ * This class is used to find the final path of using the 2 opt heuristic; uses
+ * helper class PathHeler
+ * 
+ * @see PathHelper
  */
 
 public class TwoOpt {
@@ -12,24 +15,47 @@ public class TwoOpt {
 	List<Point> route;
 	PathHelper pathHelper;
 
-	
+	/**
+	 * Class constructor
+	 * 
+	 * @param route      initial route
+	 * @param pathHelper pathHelper class to access helper method
+	 */
 	public TwoOpt(List<Point> route, PathHelper pathHelper) {
 		this.route = route;
-		size = route.size();
 		this.pathHelper = pathHelper;
+		/**
+		 * set size as size of route
+		 */
+		size = route.size();
 
 	}
 
+	/**
+	 * useing the 2 opt algorithm to find best path: set the best route to the
+	 * initial route. loop through all possible i to k within size and them and
+	 * compare the if the swapped one has lower number of steps then the best. if so
+	 * set best to new route. repeat this process until improve == 2 is reached.
+	 * 
+	 * @return best path obtained through the 2 opt algorithm
+	 * 
+	 * @see TwoOptSwap(int, int, List<Point>)
+	 * @see PathHelper#findAllSteps(List<Point>)
+	 */
 	public List<Path> findBestPath() {
 		int improve = 0;
 		List<Point> bestRoute = route;
 		while (improve < 2) {
 			for (int i = 1; i < size - 1; i++) {
 				for (int k = i + 1; k < size - 1; k++) {
+					/**
+					 * use the twoOptSwap method to get new route
+					 */
 					List<Point> newRoute = TwoOptSwap(i, k, bestRoute);
 					/**
-					 * Since the only different part of the route is the reversed part, I only
-					 * compared the steps it takes to do the route from i-1 to k+1
+					 * Since the only different part of the route is the reversed part, only
+					 * compared the steps it takes to do the route from i-1 to k+1. Obtain the path
+					 * from calling findAllSteps in pathHelper
 					 */
 					List<Path> reversed = pathHelper.findAllSteps(newRoute.subList(i - 1, k + 2));
 					List<Path> original = pathHelper.findAllSteps(bestRoute.subList(i - 1, k + 2));
@@ -43,6 +69,9 @@ public class TwoOpt {
 					}
 				}
 			}
+			/**
+			 * increase improve by one
+			 */
 			improve++;
 		}
 		/**
@@ -51,14 +80,22 @@ public class TwoOpt {
 		return pathHelper.findAllSteps(bestRoute);
 	}
 
-	
+	/**
+	 * swap the route from i to k while leaving anything else untouched and return
+	 * new route
+	 * 
+	 * @param i     start of swap
+	 * @param k     end of swap
+	 * @param route route to swap
+	 * @return swapped route
+	 */
 	private List<Point> TwoOptSwap(int i, int k, List<Point> route) {
 		/**
 		 * take route[0] to route[i-1] and add them in order to new_route
 		 */
 		List<Point> result = new ArrayList<>();
 		result.addAll(route.subList(0, i));
-		
+
 		/**
 		 * take route[i] to route[k] and add them in reverse order to new_route
 		 */
--- src/main/java/uk/ac/ed/inf/aqmaps/WriteFiles.java
+++ src/main/java/uk/ac/ed/inf/aqmaps/WriteFiles.java
@@ -16,12 +16,33 @@ import java.io.FileWriter;
 
 public class WriteFiles {
 
+	/**
+	 * Class constructor; compute the content needed for the two output files and
+	 * create/write them
+	 * 
+	 * @param finalPath the final path obtained from TwoOpt
+	 * @param data      get data stored at the start
+	 * 
+	 * @see writeToFiles
+	 * @see getStringProperty
+	 */
 	public WriteFiles(List<Path> finalPath, Data data) {
+		/**
+		 * get sensors as Point and what3word from loaded data
+		 */
 		Map<Point, String> sensors = data.sensors;
+		/**
+		 * sensors as what3words and corresponding reading and battery information
+		 */
 		Map<String, List<String>> sensorInfo = data.sensorsInfo;
 		List<Point> sensorLocations = data.sensorLocations;
-
+		/**
+		 * List of features to be converted into geoJson for output
+		 */
 		List<Feature> fl = new ArrayList<>();
+		/**
+		 * txt string of flight to be written into the file
+		 */
 		String flightPath = "";
 
 		/**
@@ -32,35 +53,70 @@ public class WriteFiles {
 			finalPath = finalPath.subList(0, 151);
 		}
 
+		/**
+		 * keep track of all the visited sensors to see if all is reached
+		 */
 		List<Point> visitedSensors = new ArrayList<>();
 
+		/**
+		 * for each path add the corresponding path in flightPath and LineString and
+		 * Point (if any sensor) to fl for output
+		 */
 		for (int i = 0; i < finalPath.size(); i++) {
 			double startLong = finalPath.get(i).getStart().coordinates().get(0);
 			double startLat = finalPath.get(i).getStart().coordinates().get(1);
 			double endLong = finalPath.get(i).getEnd().coordinates().get(0);
 			double endLat = finalPath.get(i).getEnd().coordinates().get(1);
 
+			/**
+			 * add current LineString from path to fl
+			 */
 			Feature line = Feature.fromGeometry(finalPath.get(i).getPath());
 			fl.add(line);
+			/**
+			 * for the string property "location"; initialise as the string null and if the
+			 * current path has a sensor then set to corresponding sensor
+			 */
 			String what3word = "null";
+			/**
+			 * get the string property for rbg-string, marker-color, and marker-symbol by
+			 * calling the getStringProperty method
+			 */
 			if (finalPath.get(i).getSensor() != null
 					&& sensorInfo.containsKey(sensors.get(finalPath.get(i).getSensor()))) {
+				/**
+				 * get what3word from getting the value of sensors with the Point from current
+				 * Path
+				 */
 				what3word = sensors.get(finalPath.get(i).getSensor());
 				Feature point = Feature.fromGeometry(finalPath.get(i).getSensor());
 				point.addStringProperty("location", what3word);
+				/**
+				 * get reading and battery from getting the value of sensorInfo with the
+				 * what3word obtained just now
+				 */
 				point.addStringProperty("rbg-string", getStringProperty(sensorInfo.get(what3word)).get(0));
 				point.addStringProperty("marker-color", getStringProperty(sensorInfo.get(what3word)).get(0));
 				point.addStringProperty("marker-symbol", getStringProperty(sensorInfo.get(what3word)).get(1));
 
+				/**
+				 * add the point with added string properties to fl
+				 */
 				fl.add(point);
+				/**
+				 * add sensor to the visited sensor
+				 */
 				visitedSensors.add(finalPath.get(i).getSensor());
 			}
+			/**
+			 * append the line representing the current path to flightPath
+			 */
 			flightPath += i + 1 + "," + startLong + "," + startLat + "," + finalPath.get(i).getDegree() + "," + endLong
 					+ "," + endLat + "," + what3word + "\n";
 		}
 
 		/**
-		 * add the non-visited sensors
+		 * add the non-visited sensors if any
 		 */
 		if (!visitedSensors.containsAll(sensorLocations)) {
 			System.out.println("did not reach all sensors");
@@ -77,12 +133,12 @@ public class WriteFiles {
 		}
 
 		/**
-		 * geoJson string of path and sensors as requested from the course work
+		 * convert fl into a geoJson string of path and sensors to be written into the file
 		 */
 		String geoJson = FeatureCollection.fromFeatures(fl).toJson();
 
 		/**
-		 * create and write the output files
+		 * create and write the output files from the comupted content 
 		 */
 		writeToFiles(data, flightPath, geoJson);
 
@@ -95,6 +151,18 @@ public class WriteFiles {
 		System.out.println(FeatureCollection.fromFeatures(fl).toJson());
 	}
 
+	/**
+	 * create and write the two output files
+	 * 
+	 * @param data       get data stored at the start to access date, month, year
+	 *                   for filename
+	 * @param flightPath String to write to the txt file that represents the
+	 *                   flightPath
+	 * @param geoJson    geoJson string that represents the geoJson of path and
+	 *                   sensor with corresponding properties
+	 * 
+	 * @throws IOException If an input or output exception occurred
+	 */
 	private void writeToFiles(Data data, String flightPath, String geoJson) {
 		try {
 			String pathFileName = "flightpath-" + data.date + "-" + data.month + "-" + data.year + ".txt";
@@ -119,18 +187,30 @@ public class WriteFiles {
 		}
 	}
 
-	private List<String> getStringProperty(List<String> info) {
-		String reading = info.get(0);
-		String battery = info.get(1);
+	/**
+	 * check the reading and the battery to get the corresponding string properties
+	 * of marker symbol and colour
+	 * 
+	 * @param sensorInfo
+	 * @return corresponding string properties of marker symbol and colour
+	 */
+	private List<String> getStringProperty(List<String> sensorInfo) {
+		String reading = sensorInfo.get(0);
+		String battery = sensorInfo.get(1);
 
+		/**
+		 * check if battery is low
+		 */
 		List<String> results = new ArrayList<>();
 		if (Double.parseDouble(battery) < 10) {
 			results = Arrays.asList("#000000", "cross");
 			return results;
 		}
 
+		/**
+		 * check to see which range the reading is in
+		 */
 		Double data = Double.parseDouble(reading);
-
 		if (data >= 0 && data < 32) {
 			results = Arrays.asList("#00ff00", "lighthouse");
 		} else if (data >= 32 && data < 64) {
